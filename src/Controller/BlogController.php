c6f4aa125aedd4236b784a1063da99bb70046f5e
DidierColombbus <100928062+DidierColombbus@users.noreply.github.com>
2022-09-05 15:25:02 +0200

    modif config

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\component\Form\Extension\Core\Type\TextType;
+
+
 
 class BlogController extends AbstractController
 {
@@ -34,6 +39,37 @@ class BlogController extends AbstractController
         ]);
     }
 
+    
+    /**
+ * @Route("/blog/new", name="blog_create")
+ */
+ public function create(Request $request, EntityManagerInterface $manager)
+ {
+ $article = new Article(); // nous déclarons un article qui est vide mais pret à être rempli
+ // $form est un objet complexe, nous allons demander à symfony de nous stocker le formulaire dans une variable simple à utilier.
+ $form = $this->createFormBuilder($article) // cela va créer un objet qui est lié à notre article
+ ->add('title', TextType::class, [
+    // On définit les attributs du champs 'title'
+    'attr' => [
+    'placeholder' => "Contenu de l'article"
+    ]
+    ]) // add() fonction permettant de créer des champs dans un formulaire
+    ->add('content', TextType::class, [
+        'attr' => [
+        'placeholder' => "Choix de l'image",
+        'class' => 'form-control'
+        ]
+        ])
+       
+ ->add('image')
+ ->getForm(); // permet d'afficher le rendu final
+ return $this->render('blog/create.html.twig', [
+    // createView() va retourner un petit objet qui représente l'affichage du formulaire, on le récupère sur la page create.html.twig
+    'formArticle' => $form->createView()
+    ]);
+ }
+
+
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
@@ -45,12 +81,4 @@ class BlogController extends AbstractController
             'article' => $article
         ]);
     }
-
-    /**
- * @Route("/blog/new", name="blog_create")
- */
- public function create()
- {
- $this->render('blog/create.html.twig');
- }
 }
